0f7207781e5b83747b2c4d42f7865bbb8f12cd68
alishahbaz659 <alishahbazbal1234@gmail.com>
2025-06-14 15:51:54 +0200

    project strucutre setup

---
+++ src/main/java/com/example/LogsParser/LogsParserApplication.java
@@ -0,0 +1,13 @@
+package com.example.LogsParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LogsParser/LogsParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LogsParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
